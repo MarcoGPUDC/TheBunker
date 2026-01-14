51b1f4454550ee60c913d40d42d122ad1949782f
MarcoGPUDC <mgpalavecino@udc.edu.ar>
2026-01-14 18:56:51 -0300

    horarios de actividades

--- js/horarios.js
+++ js/horarios.js
@@ -1,5 +1,19 @@
-async function cargarHorarios() {
-    const url = 'https://docs.google.com/spreadsheets/d/1DSYQ1u2NkTMMZv2jrNrYA0p-bz7je1ZBNyOZreC0Tt4/export?format=csv';
+async function cargarHorarios(actividad) {
+    var pag;
+    if(actividad=="calistenia"){
+        pag="0";
+    } else if(actividad=="funcional-chicas"){
+        pag="1638770279";
+    } else if(actividad=="funcional-mayores"){
+        pag="431847341";
+    } else if(actividad=="jiu-jitzu"){
+        pag="676925863";
+    } else if(actividad=="danzas-arabes"){
+        pag="270624033";
+    } else {
+        pag="0";
+    }
+    const url = `https://docs.google.com/spreadsheets/d/1DSYQ1u2NkTMMZv2jrNrYA0p-bz7je1ZBNyOZreC0Tt4/export?gid=${pag}&format=csv`;
     
     const res = await fetch(url);
     const csv = await res.text();
@@ -10,8 +24,8 @@ async function cargarHorarios() {
     
     const [encabezados, ...datos] = filas;
     
-    const thead = document.querySelector('#tabla-horarios thead');
-    const tbody = document.querySelector('#tabla-horarios tbody');
+    const thead = document.querySelector(`#${actividad} thead`);
+    const tbody = document.querySelector(`#${actividad} tbody`);
     
     thead.innerHTML =
         '<tr>' + encabezados.map(h => `<th>${h}</th>`).join('') + '</tr>';
@@ -21,5 +35,9 @@ async function cargarHorarios() {
     ).join('');
     }
       
-cargarHorarios();
+cargarHorarios("calistenia");
+//cargarHorarios("funcional-chicas");
+//cargarHorarios("funcional-mayores");
+//cargarHorarios("jiu-jitzu");
+//cargarHorarios("danzas-arabes");
       
